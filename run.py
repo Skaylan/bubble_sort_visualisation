f38d8969677858f676c5221b1d7a61770a8189a4
skaylan <lucas_7tv@hotmail.com>
2022-10-16 12:04:40 -0300

    draw line bug fix

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def draw_lines(l: list[int], screen: pygame.Surface, font: pygame.font) -> None:
 def sort(l: list[int], screen: pygame.Surface, clock: pygame.time.Clock, font: pygame.font) -> None:
     for i in range(len(l)):
         for j in range(len(l)-1):
-            if l[j] > l[j+1]:
+            if l[j] < l[j+1]:
                 aux = l[j]
                 l[j] = l[j+1]
                 l[j+1] = aux
